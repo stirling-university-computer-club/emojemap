02591427074816a895e806e95ef65cc286b39f00
Daniel <whereisdaniel@yahoo.co.uk>
2015-02-21 14:31:29 +0000

    Added package and launch

--- EmojeMap/src/uk/co/stircomp/emojemap/Start.java
+++ EmojeMap/src/uk/co/stircomp/emojemap/Start.java
@@ -1,10 +1,13 @@
 package uk.co.stircomp.emojemap;
 
+import uk.co.stircomp.emojemap.UI.WorldUI;
+
 public class Start {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("-- A map of world emotions --");
+		WorldUI worldUI = new WorldUI();
 		
 	}
 
---
+++ EmojeMap/src/uk/co/stircomp/emojemap/UI/WorldUI.java
@@ -0,0 +1,15 @@
+package uk.co.stircomp.emojemap.UI;
+
+import javax.swing.JFrame;
+
+public class WorldUI extends JFrame{
+
+	public WorldUI()
+	{
+		this.setSize(1024, 768);
+		this.setLocation(100, 100);
+		this.setTitle("Misery Map");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
