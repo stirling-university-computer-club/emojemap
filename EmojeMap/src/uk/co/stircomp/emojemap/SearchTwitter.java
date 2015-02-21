537802f2fe5ccaa654458bc3ca60985ceed015c6
Martynas <map00054@students.stir.ac.uk>
2015-02-21 21:19:12 +0000

    SearchTwitter update TwitterFetch update

--- EmojeMap/src/uk/co/stircomp/emojemap/SearchTwitter.java
+++ EmojeMap/src/uk/co/stircomp/emojemap/SearchTwitter.java
@@ -11,30 +11,30 @@ import twitter4j.api.SearchResource;
 
 public class SearchTwitter implements SearchResource
 {
-
-	public SearchTwitter() throws TwitterException {
+	
+	static Twitter twitter = TwitterFactory.getSingleton();
+	
+	public static QueryResult search(String word) throws TwitterException{
 		
 		// The factory instance is re-useable and thread safe.
-	    Twitter twitter = TwitterFactory.getSingleton();
-	    Query query = new Query("angry");
+	    
+	    Query query = new Query(word);
 	    QueryResult result = twitter.search(query);
-	    for (twitter4j.Status status : result.getTweets()) {
-	    	
-	        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-	        
-	    }	
 		
-	}
-	
-	
-	@Override
-	public QueryResult search(Query q) throws TwitterException {
-		return null;
+		return result;
 	}
 
 	public static void query(String q) {
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public QueryResult search(Query arg0) throws TwitterException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 	
 }
